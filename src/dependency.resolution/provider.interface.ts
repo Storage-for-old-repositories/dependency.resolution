d6b12515f6a7a23cd6a3848c20bdafc1ca13c2e5
Kirill Midzyaev <givename48@yandex.ru>
2023-05-14 16:18:44 +0500

    add: Provider interface

---
+++ src/dependency.resolution/provider.interface.ts
@@ -0,0 +1,1 @@
+export default interface IProvider<_Provide = any> {}
--- src/dependency.resolution/provider.ts
+++ src/dependency.resolution/provider.ts
@@ -1,72 +1,90 @@
-type ProviderDependenciesBound = Record<string, Provider>;
+import IProvider from "./provider.interface";
+
+type ProviderDependenciesBound = Record<string, IProvider>;
 
 type ProviderDependenciesResolution<Dependencies> =
   Dependencies extends ProviderDependenciesBound
     ? {
-        [K in keyof Dependencies]: Dependencies[K] extends Provider<infer R>
+        [K in keyof Dependencies]: Dependencies[K] extends IProvider<infer R>
           ? R
           : never;
       }
     : never;
 
-type ProviderResolver<Dependencies, Provide> = (
-  dependencies: Dependencies
+type ProviderResolver<Provide, Dependencies, Config> = (
+  dependencies: Dependencies,
+  config: Config
 ) => Provide | Promise<Provide>;
 
-export interface ProviderOptionsFrom {
-  /** @default */
+export interface ProviderOptions<Config> {
+  /** @default true */
   isSingleton: boolean;
+  config: Config;
 }
 
-export interface ProviderOptionsCreate<Dependencies> {
+export interface ProviderSharedOptions<Dependencies> {
   dependencies: Dependencies;
 }
 
 export default class Provider<
-  _Provide = any,
+  Provide = any,
   Dependencies = any,
-  Resolver = any
-> {
+  Resolver = any,
+  Config = void
+> implements IProvider<Provide>
+{
   private _isSingleton = true;
+  private _config: Readonly<Config> = undefined!;
 
   private constructor(
     private readonly _dependencies: Readonly<Dependencies>,
     private readonly _resolver: Resolver
   ) {}
 
-  public static create<Provide, Dependencies extends ProviderDependenciesBound>(
-    options: ProviderOptionsCreate<Dependencies> & Partial<ProviderOptionsFrom>,
+  public static create<
+    Provide,
+    Dependencies extends ProviderDependenciesBound,
+    Config = void
+  >(
+    options: ProviderSharedOptions<Dependencies> &
+      Partial<ProviderOptions<Config>>,
     resolver: ProviderResolver<
+      Provide,
       ProviderDependenciesResolution<Dependencies>,
-      Provide
+      Config
     >
   ) {
     const dependencies = { ...options.dependencies };
-    const provider = new Provider<Provide, Dependencies, typeof resolver>(
-      dependencies,
-      resolver
-    );
+    const provider = new Provider<
+      Provide,
+      Dependencies,
+      typeof resolver,
+      Config
+    >(dependencies, resolver);
     provider.applyOptionsFrom(options);
-    return provider;
+    return provider as IProvider<Provide>;
   }
 
-  private applyOptionsFrom(options: Partial<ProviderOptionsFrom>) {
+  private applyOptionsFrom(options: Partial<ProviderOptions<Config>>) {
+    this._config = options.config!;
     if (typeof options.isSingleton === "boolean") {
       this._isSingleton = options.isSingleton;
     }
   }
 
-  public from(options: Partial<ProviderOptionsFrom>) {
-    const provider = new Provider<_Provide, Dependencies, Resolver>(
+  public create(options?: Partial<ProviderOptions<Config>>) {
+    const provider = new Provider<Provide, Dependencies, Resolver, Config>(
       this._dependencies,
       this._resolver
     );
-    provider.applyOptionsFrom(options);
-    return provider;
+    if (options) {
+      provider.applyOptionsFrom(options);
+    }
+    return provider as IProvider<Provide>;
   }
 
   public get dependencies() {
-    return { ...this._dependencies };
+    return this._dependencies;
   }
 
   public get resolver() {
@@ -76,4 +94,32 @@ export default class Provider<
   public get isSingleton() {
     return this._isSingleton;
   }
+
+  public get config() {
+    return this._config;
+  }
 }
+
+// const provide = Provider.create(
+//   {
+//     dependencies: {
+//       file: Provider.create(
+//         {
+//           dependencies: {},
+//         },
+//         async () => "hello"
+//       ),
+//       auto: Provider.create(
+//         {
+//           dependencies: {},
+//         },
+//         async () => [["hello"]]
+//       ),
+//       urb: {} as IProvider<1 | 2>,
+//     },
+//     config: {
+//       work: "sdf",
+//     },
+//   },
+//   async (d, c) => {}
+// );
